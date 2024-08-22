bf556f4e7ae4af12acbf35632635a217d13b03c4
Monatric <nawpnawp8@gmail.com>
2024-08-23 06:48:16 +0800

    Somehow make iterative and recursive fibs work

---
+++ fibonacci.rb
@@ -0,0 +1,22 @@
+def fibs(n)
+  result = [0, 1]
+  return result = [0] if n < 2
+
+  (n - 2).times do
+    result << (result[-1] + result[-2])
+  end
+  result
+end
+
+p fibs(8)
+
+def fibs_rec(n, result = [0, 1])
+  if result.size == n
+    result
+  else
+    result << (result[-1] + result[-2])
+    fibs_rec(n, result)
+  end
+end
+
+p fibs_rec(8)
